e122f9f86d0c820c65ef5a62b51671f5c6e92612
Gokul <gokulsenpai@gmail.com>
2020-07-19 22:06:00 +0530

    Included Scorm
    Tried Scorm. Send help pls.

---
+++ Assets/SaveLoad.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SaveLoad : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public string theText;
+    public TMP_Text note;
+    public TMP_InputField placeHolder;
+
+    private void Start()
+    {
+        theText = PlayerPrefs.GetString("NoteContents");
+        placeHolder.text = theText;
+    }
+
+    public void SaveNote()
+    {
+        theText = note.text;
+        PlayerPrefs.SetString("NoteContents", theText);
+    }
+}
