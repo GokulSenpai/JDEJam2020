09539c5deebb5386d14731fb4e6b450e44fff285
Goku <gokulsenpai@gmail.com>
2020-09-26 15:37:41 +0530

    Added SetPassword UI and functionality

--- Assets/PasswordProtection.cs
+++ Assets/PasswordProtection.cs
@@ -6,10 +6,32 @@ using UnityEngine.UI;
 
 public class PasswordProtection : MonoBehaviour
 {
-    public TMP_InputField inputField;
-
-    private void PasswordChecker()
+    public TMP_InputField inputPassword;
+    public TMP_Text wiredTitle;
+    
+    private const string Password = "Password";
+    private string m_wiredTitleBackup = "The Wired";
+    public void PasswordChecker()
     {
-        
+        if (PlayerPrefs.GetString(Password) == inputPassword.text)
+        {
+            // Go to next menu
+        }
+        else
+        {
+            Debug.Log("Wrong Password");
+            
+            wiredTitle.text = m_wiredTitleBackup;
+            gameObject.GetComponent<LetterRandomize>().enabled = false;
+            wiredTitle.text = "Wrong";
+            StartCoroutine(SetWiredTitleBack());
+        }
+    }
+
+    private IEnumerator SetWiredTitleBack()
+    {
+        gameObject.GetComponent<LetterRandomize>().enabled = true;
+        yield return new WaitForSeconds(7);
+        wiredTitle.text = m_wiredTitleBackup;
     }
 }
---
+++ Assets/SetNewPassword.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetNewPassword : MonoBehaviour
+{
+    public TMP_InputField enteredPassword;
+    public GameObject passwordSetUi;
+    private const string Password = "Password";
+
+    public void OnSetPasswordButtonClick()
+    {
+        PlayerPrefs.SetString(Password, enteredPassword.text);
+        enteredPassword.text = "";
+        Debug.Log("Password Set Successfully and the password is " + PlayerPrefs.GetString(Password));
+       
+        passwordSetUi.SetActive(true);
+    }
+}
