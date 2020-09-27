337e17e512d9f8804b98126269a4ba1d8c8df647
Goku <gokulsenpai@gmail.com>
2020-09-27 22:43:27 +0530

    Implemented Save System for 1
    Needs Testing! A LOT! Check those using( ) lines especially

---
+++ Assets/Scripts/Save/DiaryEntryData.cs
@@ -0,0 +1,15 @@
+﻿namespace Save
+{
+    [System.Serializable]
+    public class DiaryEntryData
+    {
+        public string entryHeading;
+        public string entryInformation;
+
+        public DiaryEntryData(DiaryEntryContentPage dataSave)
+        {
+            entryHeading = dataSave.entryTitle.text;
+            entryInformation = dataSave.entryContent.text;
+        }
+    }
+}
---
+++ Assets/Scripts/Save/SaveSystem.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Save
+{
+    public static class SaveSystem
+    {
+        public static void SaveEntry(DiaryEntryContentPage dataSave)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            string path = Path.Combine(Application.persistentDataPath, "DearDiary.love");
+
+            using (FileStream stream = new FileStream(path, FileMode.Create))
+            {
+                DiaryEntryData data = new DiaryEntryData(dataSave);
+            
+                formatter.Serialize(stream, data);
+                //stream.Close();
+            }
+        }
+
+        public static DiaryEntryData LoadEntry()
+        {
+            string path = Path.Combine(Application.persistentDataPath, "DearDiary.love");
+            if (File.Exists(path))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                
+                using (FileStream stream = new FileStream(path, FileMode.Open))
+                {
+                    DiaryEntryData data = formatter.Deserialize(stream) as DiaryEntryData;
+                    //stream.Close();
+                
+                    return data;
+                }
+            }
+            else
+            {
+                Debug.Log("Save File Not Found in " + path);
+                return null;
+            }
+        }
+    }
+}
