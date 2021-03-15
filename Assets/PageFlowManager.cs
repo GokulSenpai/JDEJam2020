4bf6ae3886fe4bd033a1c2e12577f566e6f7f356
Goku <47669557+GokulSenpai@users.noreply.github.com>
2021-03-15 06:44:04 +0530

    Page Flow implemented
    Implemented a centralized password manager

---
+++ Assets/PageFlowManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PageFlowManager : MonoBehaviour
+{
+    [SerializeField] private GameObject mainPage;
+    [SerializeField] private GameObject setPasswordPage;
+    [SerializeField] private GameObject passwordSetPage;
+    [SerializeField] private GameObject enterPasswordPage;
+    [SerializeField] private GameObject wrongPasswordPage;
+    [SerializeField] private GameObject diaryListPage;
+    [SerializeField] private GameObject diaryContentPage;
+    
+    public void MainPageButtonClicked()
+    {
+        if (PlayerPrefs.HasKey("Password"))
+        {
+            enterPasswordPage.SetActive(true);
+        }
+        else
+        {
+            setPasswordPage.SetActive(true);
+        }
+        
+        mainPage.SetActive(false);
+    }
+
+    public void PasswordSet()
+    {
+        StartCoroutine(TimeoutPasswordSetScreenCoroutine());
+        setPasswordPage.SetActive(false);
+        diaryListPage.SetActive(true);
+    }
+
+    private IEnumerator TimeoutPasswordSetScreenCoroutine()
+    {
+        passwordSetPage.SetActive(true);
+        yield return new WaitForSeconds(3);
+        passwordSetPage.SetActive(false);
+    }
+    
+    
+    public void CorrectPassword()
+    {
+        Debug.Log("Correct Password");
+        enterPasswordPage.SetActive(false);
+        diaryListPage.SetActive(true);
+    }
+
+    public void WrongPassword()
+    {
+        Debug.Log("Wrong Password");
+        StartCoroutine(TimeOutWrongPasswordScreenCoroutine());
+    }
+
+    private IEnumerator TimeOutWrongPasswordScreenCoroutine()
+    {
+        wrongPasswordPage.SetActive(true);
+        yield return new WaitForSeconds(3);
+        wrongPasswordPage.SetActive(false);
+    }
+
+    public void DiaryListClick()
+    {
+        diaryContentPage.SetActive(true);
+    }
+}
---
+++ Assets/PasswordManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PasswordManager : MonoBehaviour
+{
+    public TMP_InputField setPassword;
+    private const string Password = "Password";
+    
+    public TMP_InputField enteredPassword;
+
+    private PageFlowManager m_pageFlowManager;
+
+    private void Awake()
+    {
+        m_pageFlowManager = FindObjectOfType<PageFlowManager>();
+    }
+
+    public void OnSetPasswordButtonClick()
+    {
+        PlayerPrefs.SetString(Password, setPassword.text);
+        setPassword.text = "";
+        Debug.Log("Password Set Successfully and the password is " + PlayerPrefs.GetString(Password));
+       
+        m_pageFlowManager.PasswordSet();
+    }
+    
+    public void PasswordChecker()
+    {
+        if (PlayerPrefs.GetString(Password) == enteredPassword.text)
+        {
+            m_pageFlowManager.CorrectPassword();
+        }
+        else
+        {
+            m_pageFlowManager.WrongPassword();
+        }
+    }
+}
